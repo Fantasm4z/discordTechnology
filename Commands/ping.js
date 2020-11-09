3c4c5900480d7a9868a739eb210f455ca746bedd
Fantasm4z <fantasm4@outlook.com.br>
2020-11-09 16:38:03 -0300

    changed delete to edit in ping.js

--- Commands/ping.js
+++ Commands/ping.js
@@ -10,8 +10,8 @@ const command = {
         const vinDiesel = client.emojis.cache.get ( '704808448155451434' );
         
         msg.channel.send( `Pingando... ${ vinDiesel }` ).then ( async ( message ) => {
-            message.delete ( )
-            msg.channel.send ( `Ping: **${ message.createdTimestamp - msg.createdTimestamp }ms**.\nPing REST-API: **${ Math.round ( client.ws.ping ) }ms**` );
+            //message.delete ( )
+            message.edit ( `Ping: **${ message.createdTimestamp - msg.createdTimestamp }ms**.\nPing REST-API: **${ Math.round ( client.ws.ping ) }ms**` );
         });
 
     }
